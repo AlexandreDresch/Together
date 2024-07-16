492231c70f083c6f4abcd550af99543653cffda8
AlexandreDresch <connectWithAlexandre@proton.me>
2024-07-16 00:20:04 -0300

    feat: add DatePicker

--- src/components/steps/destination-and-date-step.tsx
+++ src/components/steps/destination-and-date-step.tsx
@@ -5,6 +5,10 @@ import {
   Settings2Icon,
 } from "lucide-react";
 import Button from "../shared/button";
+import { useState } from "react";
+import { DateRange, DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import Modal from "../shared/modal";
 
 interface DestinationAndDateStepProps {
   isGuestsInputOpen: boolean;
@@ -17,6 +21,17 @@ export default function DestinationAndDateStep({
   isGuestsInputOpen,
   openGuestsInput,
 }: DestinationAndDateStepProps) {
+  const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+  const [selectedDays, setSelectedDays] = useState<DateRange | undefined>();
+
+  function openDatePicker() {
+    setIsDatePickerOpen(true);
+  }
+
+  function closeDatePicker() {
+    setIsDatePickerOpen(false);
+  }
+
   return (
     <div className="flex h-16 items-center gap-3 rounded-xl bg-zinc-900 px-4 shadow-shape">
       <div className="flex flex-1 items-center gap-2">
@@ -29,15 +44,27 @@ export default function DestinationAndDateStep({
         />
       </div>
 
-      <div className="flex items-center gap-2">
-        <Calendar className="size-5 text-zinc-400" />
-        <input
-          type="text"
-          placeholder="When?"
-          className="w-40 bg-transparent text-lg outline-none placeholder:text-zinc-400"
-          disabled={isGuestsInputOpen}
+      <button
+        className="flex items-center gap-2 text-left text-zinc-400"
+        disabled={isGuestsInputOpen}
+        onClick={openDatePicker}
+      >
+        <Calendar className="size-5" />
+        <span>When?</span>
+      </button>
+
+      <Modal
+        isOpen={isDatePickerOpen}
+        onClose={closeDatePicker}
+        title="Select the date"
+      >
+        <DayPicker
+          mode="range"
+          selected={selectedDays}
+          onSelect={setSelectedDays}
+          
         />
-      </div>
+      </Modal>
 
       <div className="h-6 w-px bg-zinc-800" />
 
