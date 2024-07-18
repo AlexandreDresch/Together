f7d308a7770dba6bd812c7b2404a6016724bf564
AlexandreDresch <connectWithAlexandre@proton.me>
2024-07-17 23:22:59 -0300

    feat: add event & guest interfaces to global types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,27 @@
 export interface CreateEventProps {
-    destination: string;
-    starts_at: string;
-    ends_at: string;
-    emails_to_invite: string[];
-    owner_name: string;
-    owner_email: string;
+  destination: string;
+  starts_at: string;
+  ends_at: string;
+  emails_to_invite: string[];
+  owner_name: string;
+  owner_email: string;
+}
+
+export interface CreateEventResponse {
+  eventId: string;
+}
+
+export interface Event {
+  id: string;
+  destination: string;
+  starts_at: string;
+  ends_at: string;
+  is_confirmed: boolean;
+}
+
+export interface Guest {
+  id: string;
+  name: string | null;
+  email: string;
+  is_confirmed: boolean;
 }
