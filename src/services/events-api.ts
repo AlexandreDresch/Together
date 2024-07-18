5469c1d402cae9f62f2c9fd0f459b11269433817
AlexandreDresch <connectWithAlexandre@proton.me>
2024-07-17 23:23:37 -0300

    feat: add getEventById & getGuestsByEventId to events api

--- src/services/events-api.ts
+++ src/services/events-api.ts
@@ -1,8 +1,22 @@
-import { CreateEventProps } from "../types";
+import { CreateEventProps, CreateEventResponse, Event, Guest } from "../types";
 import { api } from "./api";
 
-export async function postEvent(createEventData: CreateEventProps) {
+export async function postEvent(
+  createEventData: CreateEventProps,
+): Promise<CreateEventResponse> {
   const response = await api.post("/events", { createEventData });
+
+  return response.data;
+}
+
+export async function getEventById(id: string): Promise<Event> {
+  const response = await api.get(`/events/${id}`);
+
+  return response.data;
+}
+
+export async function getGuestsByEventId(eventId: string): Promise<Guest[]> {
+  const response = await api.get(`/events/${eventId}/participants`);
 
   return response.data;
 }
